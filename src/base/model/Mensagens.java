a6373617353fd36980e90eb2684a0f3b99ea09b5
Yamesh <seupera@hotmail.com>
2018-07-08 16:00:59 -0300

    Cliente interage com o usuário.
    Ele recebe comandos com ZERO ou UM argumentos, e pode processá-los através de funções lambda.

--- src/base/model/Mensagens.java
+++ src/base/model/Mensagens.java
@@ -1,9 +1,16 @@
 package base.model;
 
 public enum Mensagens {
-	OLA_CLIENTE("Bem vindo ao cliente. Digite \"help\", para"
-			+ " obter a lista de comandos, ou digite o comando desejado."), 
-	COMANDO_DESCONHECIDO("Desculpa. Não entendi o que você deseja fazer.");
+	OLA_CLIENTE(
+			"Bem vindo ao cliente. Digite \"help\", para obter a lista \n"
+			+ "de comandos, ou digite o comando desejado."
+					), 
+	COMANDO_DESCONHECIDO(
+			"Desculpa. Não entendi o que você deseja fazer."
+			), 
+	COMANDO_COM_MUITOS_ARGUMENTOS(
+			"Você digitou muita coisa. Nossos comandos não recebem tantos argumentos."
+			);
 	
 	
 	
