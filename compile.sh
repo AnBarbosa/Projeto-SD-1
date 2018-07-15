9421daf7f2387e3f3491d999e78f2e7dec90d304
Andre <Andre>
2018-07-15 20:12:09 -0300

    Adicionado comando para compilar no Linux

---
+++ compile.sh
@@ -0,0 +1,17 @@
+
+javac util/*.java -d ./../bin/
+
+javac base/model/interfaces/*.java -d ./../bin/
+
+javac servidor/model/*.java -d ./../bin/
+
+javac cliente/controller/*.java -d ./../bin/
+javac cliente/dao/*.java -d ./../bin/
+javac cliente/interfaces/anotacoes/*.java -d ./../bin/
+javac cliente/interfaces/*.java -d ./../bin/
+javac cliente/auxiliar/*.java -d ./../bin/
+javac cliente/view/*.java -d ./../bin/
+javac cliente/*.java -d ./../bin/
+
+
+
